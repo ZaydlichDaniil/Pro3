8fea380e7d847994a862aee867e2da4325a4014c
ZaydlichDaniil <daniil.zaidlich@nure.ua>
2025-06-06 14:05:12 +0300

    lb 1

--- lb 2/lb 2 task 2.py
+++ lb 2/lb 2 task 2.py
@@ -13,15 +13,15 @@ def generate_file_hashes(*file_paths):
                 file_hash = hashlib.sha256(file_data).hexdigest()
                 hashes[path] = file_hash
         except FileNotFoundError:
-            print(f"❌ Помилка: файл '{path}' не знайдено.")
+            print(f" Помилка: файл '{path}' не знайдено.")
         except IOError:
-            print(f"❌ Помилка: не вдалося прочитати файл '{path}'.")
+            print(f" Помилка: не вдалося прочитати файл '{path}'.")
 
     return hashes
 
 # Приклад використання
 hashes = generate_file_hashes("file1.txt", "file2.txt", "apache_logs.txt")
 
-print("\n✅ SHA-256 хеші файлів:")
+print("\n SHA-256 хеші файлів:")
 for file, h in hashes.items():
     print(f"{file}: {h}")
